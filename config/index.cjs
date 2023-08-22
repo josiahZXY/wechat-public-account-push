1b6b9c5b3e4beaaccd45781e5fd8107a9a7d1e00
josiahZXY <shouweiren@126.com>
2023-08-23 00:09:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oPiqc6KY8fjyKCARrmGUtLElv8F4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'k-hmMvzUtq3yj3WQuwqWEdWA7e3maLhY-_MLqtQa6yY',
+      useTemplateId: 'el2VBf37IxECISpbdclyeu5Thu1poSjPprzmwp6q8EA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-03',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'k-hmMvzUtq3yj3WQuwqWEdWA7e3maLhY-_MLqtQa6yY',
+  CALLBACK_TEMPLATE_ID: 'el2VBf37IxECISpbdclyeu5Thu1poSjPprzmwp6q8EA',
 
   CALLBACK_USERS: [
     {
