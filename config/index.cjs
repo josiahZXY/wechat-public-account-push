777c3f0909421125c20aa26ee5f917fc058e2662
josiahZXY <shouweiren@126.com>
2023-08-23 00:18:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '拖子',
+      name: 'ZvEI',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPiqc6KY8fjyKCARrmGUtLElv8F4',
+      id: 'oPiqc6EMdRdZdFPk6zYQ8flgg9M8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'mvcyPLX1PXINcyp3VCjrElgLSefO0PaHIiVp4vGK2M0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
